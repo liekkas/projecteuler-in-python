808c62d1c21d84d4509d12031273c19a67eeee0b
liekkas.zeng <zengjiangqin@gmail.com>
2012-06-16 16:59:27 +0800

    p10

--- mymath.py
+++ mymath.py
@@ -59,6 +59,23 @@ def isPrime(number):
                 return False;
     return True;
 
+# 统计小于指定值下所有的素数之和，效率很高
+def sumPrime(n):
+    mark = ['0'] * ((n / 2) - 1)
+    s = 2
+    p = 3
+    while p * p < n:
+        if mark[(p / 2) - 1] == '0':
+            v = (p / 2) - 1 + p
+            s += p
+            while v < len(mark):
+                mark[v] = '1'
+                v += p
+        p += 2
+    for i in range((p / 2) - 1, len(mark)):
+        if mark[i] == '0':
+            s += ((i + 1) * 2) + 1
+    return s
     
     
 
---
+++ p10.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+# 原题连接: http://projecteuler.net/problem=10
+# @author liekkas.zeng
+# @date 2012-6-16 16:45:44
+#求和：200,0000以内的素数之和
+
+from mymath import sumPrime
+
+#采用筛选算法，结果为：142913828922，用时0.31s
+print sumPrime(2000000)		
